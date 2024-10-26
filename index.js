df15218948ea9543b1fb067512bbee2d71c17553
abdelrazekAli <dev.abdelrazek@gmail.com>
2024-10-26 18:39:48 +0300

    add rate limiting

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const mongoSanitize = require("express-mongo-sanitize")
 
 // Server INIT
@@ -15,6 +16,14 @@ app.use(cors());
 app.use(express.json());
 app.use(mongoSanitize());
 app.use(morgan("common"));
+
+// Apply rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+});
+app.use(limiter);
 
 // Websocket INIT
 const socket = require("socket.io");
